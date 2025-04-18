6981fda67feec4d3d7afd5240abaa67c9394074d
Skaishgggg <swakaish444@gmail.com>
2025-04-18 11:20:10 +0530

    second committt

--- Route/router.js
+++ Route/router.js
@@ -2,4 +2,6 @@ const express = require('express')
 const router = new express.Router()
 const userController = require('../controller/userController')
 // register
-router.post('/user/register')
+router.post('/user/register',userController.register)
+
+module.exports = router
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config()  
 const express = require('express')
 const cors = require('cors')
-
+const router = require('./Route/router')
 // create an express application
 const pfsever = express()
 
@@ -11,12 +11,16 @@ const pfsever = express()
 
 pfsever.use(cors())
 pfsever.use(express.json())
-
+pfsever.use(router)
  const PORT = 4000 || process.env.PORT
 
  pfsever.listen(PORT,()=>{
     console.log(`project fair server started at port :${PORT} and waiting for client request`);
     
+ })
+ pfsever.post('/',(req,res)=>{
+res.send(`<h1>project fair server started and waiting client request</h1>`)
+  
  })
 
  
