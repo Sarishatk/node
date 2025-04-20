aa1219244750f2765dac04e4242aa69b660129de
Skaishgggg <swakaish444@gmail.com>
2025-04-20 19:38:01 +0530

    4th

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,25 @@
+const users = require('../models/userSchema')
+
+
 // logic to resliove register
-exports.register = (req,res)=>{
+exports.register =async (req,res)=>{
     console.log("inside register controller function");
-    res.status(200).json("Register request recieved")
+    const {username,email,password} = req.body
+    // console.log(`${username},${email},${password}`);
+    // await nu oru seen nd res kittaam kittathe irikkam soo run time error ne enagne slove cheyyam vecha using try catch block
+    try{const existingUser = await  users.findOne({email})
+    if(existingUser){
+        req.status(406).json("Account already exist!! please Login.")
+    }else{
+     const newUser = new users({
+        username,email,password,github:"",linkedin:"",profile:""
+     })   
+    //  mongodb lu veraan
+    await newUser.save()
+    
+    res.status(200).json(newUser)
+    }}
+    catch(err){
+        res.status(401).json(`register API failed,Error: ${err}`)
+    }
 }
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,20 +1,41 @@
-const mongoose =  require('mongoose')
+const mongoose = require('mongoose')
+const validator = require('validator')
 
-
-const schema = new mongoose.schema({
-    username : {
-        type : String,
-        require:true,
-        min : [3,'must be atleast 3']
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        min: [3, 'must be atleast 3']
     },
-    email : {
-        type : String,
-        require:true,
-        unique:true,
-       validator(value){
-        if(!validator.isEmail(value)){
-            throw new Error("invalid Email")
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        validator(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("invalid Email")
+            }
         }
-       }
+    },
+    password: {
+
+        type: String
+
+    },
+    github: {
+
+       
+        type: String
+    },
+    linkedin: {
+
+        type: String
+
+    },
+    profile :{
+        type: String
     }
 })
+
+const user = mongoose.model("users",userSchema)
+module.exports = user
