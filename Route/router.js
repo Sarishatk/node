ec6f08d028fc107fd3a245bb4e8cc45eda4b5f43
Skaishgggg <swakaish444@gmail.com>
2025-04-22 12:58:38 +0530

    f4

--- Route/router.js
+++ Route/router.js
@@ -3,5 +3,5 @@ const router = new express.Router()
 const userController = require('../controller/userController')
 // register
 router.post('/user/register',userController.register)
-
+router.post('/users/login',userController.login)
 module.exports = router
--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,5 @@
 const users = require('../models/userSchema')
-
+const jwt = require('jsonwebtoken')
 
 // logic to resliove register
 exports.register =async (req,res)=>{
@@ -9,7 +9,7 @@ exports.register =async (req,res)=>{
     // await nu oru seen nd res kittaam kittathe irikkam soo run time error ne enagne slove cheyyam vecha using try catch block
     try{const existingUser = await  users.findOne({email})
     if(existingUser){
-        req.status(406).json("Account already exist!! please Login.")
+        res.status(406).json("Account already exist!! please Login.")
     }else{
      const newUser = new users({
         username,email,password,github:"",linkedin:"",profile:""
@@ -22,4 +22,22 @@ exports.register =async (req,res)=>{
     catch(err){
         res.status(401).json(`register API failed,Error: ${err}`)
     }
+}
+
+exports.login =async (req,res)=>{
+    console.log("inside login function")
+    const {email,password} = req.body
+    try{
+const existingUser = await users.findOne({email,password})
+ if(existingUser){
+    const token = jwt.sign({userId:existingUser._id},"supercreatekey1234")
+    res.status(200).json({existingUser,token})
+ }else{
+    res.status(404).json("Incorrect Password/Email")
+ }
+    }catch(err){
+        res.status(401).json(`login API failed,Error: ${err}`)
+    }
+
+    
 }
