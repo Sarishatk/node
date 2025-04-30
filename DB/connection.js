c3df31a061af56ddd7d305d3e8a59321c290c911
Skaishgggg <sarishakrishna58@gmail.com>
2025-04-30 20:56:39 +0530

    connection

--- DB/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const connectionString = process.env.DATABASE
-
-mongoose.connect(connectionString).then(() => {
-    console.log("successfully connected with mongodb");
-}).catch((err) => {
-    console.log(`connection failed :${err}`);
-})
