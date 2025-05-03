406c549514d3d8f131eb556093224705c1e03cef
Skaishgggg <sarishakrishna58@gmail.com>
2025-05-03 12:54:57 +0530

    middleware updation

--- MiddleWares/jwtMiddleware.js
+++ MiddleWares/jwtMiddleware.js
@@ -5,7 +5,10 @@ const jwtmiddleware =(req,res,next)=>{
     console.log('inside jwtmiddleware');
     const token = req.headers['authorization'].split(" ")[1]
     // console.log(token);
-    
+    // token ne verify cheyyan oru predefined method und athu oru variable lek edth vekkanm
+    const jwtResponse = jwt.verify(token,"supercreatekey1234")
+    console.log(jwtResponse);
+   req.payload = jwtResponse.userId 
 } 
 module.exports = jwtmiddleware
 
