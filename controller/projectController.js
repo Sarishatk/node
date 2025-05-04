48ecd29113d6dc464f7408d1cd8f2b8790cf1710
Skaishgggg <sarishakrishna58@gmail.com>
2025-05-04 10:47:42 +0530

    updated

--- controller/projectController.js
+++ controller/projectController.js
@@ -5,10 +5,8 @@ exports.addProject = (req,res)=>{
     console.log('inside add project function');
     const userId = req.payload
     const projectImage = req.file.filename
-    console.log(projectImage);
-    
-    console.log(`${userId}`);
-    
+    const {title,language,overview,github,website}=req.body
+    console.log(`${title},${language},${overview},${github},${website},${projectImage},${userId}`);
     res.status(200).json('add Project request resloved')
     
 }
