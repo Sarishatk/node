2da827cf694010e91abd388cc08e4010c455b791
Skaishgggg <sarishakrishna58@gmail.com>
2025-05-04 09:55:35 +0530

    multer update

--- Route/router.js
+++ Route/router.js
@@ -3,11 +3,12 @@ const router = new express.Router()
 const userController = require('../controller/userController')
 const projectController = require('../controller/projectController')
 const jwtmiddleware = require('../MiddleWares/jwtMiddleware')
+const multerConfig = require('../MiddleWares/multerMiddleWre')
 
 // register
 router.post('/user/register',userController.register)
 router.post('/users/login',userController.login)
 // add project
-router.post('/project/add',jwtmiddleware, projectController.addProject)
+router.post('/project/add',jwtmiddleware,multerConfig.single('projectImage'), projectController.addProject)
 // export router
 module.exports = router 
