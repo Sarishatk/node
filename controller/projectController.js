d3d15d2ff45124f909432fbc95c7dd12d14f9a96
Skaishgggg <sarishakrishna58@gmail.com>
2025-05-05 10:01:11 +0530

    updation7

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,12 +1,36 @@
+const projects = require('../models/projectSchema');
 const projectController = require('../models/projectSchema')
 
 // logic to add project
-exports.addProject = (req,res)=>{
+exports.addProject =async (req,res)=>{
     console.log('inside add project function');
     const userId = req.payload
     const projectImage = req.file.filename
     const {title,language,overview,github,website}=req.body
-    console.log(`${title},${language},${overview},${github},${website},${projectImage},${userId}`);
-    res.status(200).json('add Project request resloved')
+    // console.log(`${title},${language},${overview},${github},${website},${projectImage},${userId}`);
+    try {
+        const existingProject = await projects.findOne({ github });
+        
+        if (existingProject) {
+          res.status(406).json("project already exist !!! upload another");
+        }else{
+    
+        const newProject = new projects({
+            title,
+            language,
+            overview,
+            github,
+            projectImage,
+            userId,
+            website
+        });
+    
+        await newProject.save();
+        res.status(201).json(newProject);  // 201 Created
+    }
+    } catch (err) {
+        console.error(err); // helpful for debugging
+        res.status(500).json({ message: 'Request failed', error: err.message });
+    }
     
 }
