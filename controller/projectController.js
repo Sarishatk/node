26529364624663ddef84ef32da600d99558a6d85
Skaishgggg <sarishakrishna58@gmail.com>
2025-05-11 12:42:20 +0530

    ecf v

--- controller/projectController.js
+++ controller/projectController.js
@@ -52,6 +52,7 @@ exports.alluserProject = async(req,res)=>{
 
 exports.getAllProjects = async (req,res)=>{
     const searchkey = req.query.serach
+    
     try{
 
   const allProjects = await projects.find()
