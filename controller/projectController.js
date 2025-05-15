f4890df27c474fcc1bd1c0242ffbca60b3a49e5a
Skaishgggg <sarishakrishna58@gmail.com>
2025-05-15 19:47:41 +0530

    m3

--- controller/projectController.js
+++ controller/projectController.js
@@ -2,77 +2,75 @@ const projects = require('../models/projectSchema');
 const projectController = require('../models/projectSchema')
 
 // logic to add project
-exports.addProject =async (req,res)=>{
+exports.addProject = async (req, res) => {
     console.log('inside add project function');
     const userId = req.payload
     const projectImage = req.file.filename
-    const {title,language,overview,github,website}=req.body
+    const { title, language, overview, github, website } = req.body
     // console.log(`${title},${language},${overview},${github},${website},${projectImage},${userId}`);
     try {
         const existingProject = await projects.findOne({ github });
-        
+
         if (existingProject) {
-          res.status(406).json("project already exist !!! upload another");
-        }else{
-    
-        const newProject = new projects({
-            title,
-            language,
-            overview,
-            github,
-            projectImage,
-            userId,
-            website
-        });
-    
-        await newProject.save();
-        res.status(201).json(newProject);  // 201 Created
-    }
+            res.status(406).json("project already exist !!! upload another");
+        } else {
+
+            const newProject = new projects({
+                title,
+                language,
+                overview,
+                github,
+                projectImage,
+                userId,
+                website
+            });
+
+            await newProject.save();
+            res.status(201).json(newProject);  // 201 Created
+        }
     } catch (err) {
         console.error(err); // helpful for debugging
         res.status(500).json({ message: 'Request failed', error: err.message });
     }
-    
+
 }
 
 // get user project
 
-exports.alluserProject = async(req,res)=>{
+exports.alluserProject = async (req, res) => {
     const userId = req.payload
-   try{
-    const userProjects = await projects.findOne({userId})
-    res.status(200).json(userProjects)
-   }catch(err){
-    res.status(401).json(err)
-   } 
+    try {
+        const userProjects = await projects.findOne({ userId })
+        res.status(200).json(userProjects)
+    } catch (err) {
+        res.status(401).json(err)
+    }
 }
 
 
 // get all projects
 
-exports.getAllProjects = async (req,res)=>{
-    const searchkey = req.query.serach
-    const query ={
-        language:{$regex:searchkey ,$options:"i"
-// case sensitive aavnd irikkaaan
-        }
-    }
-    try{
+exports.getAllProjects = async (req, res) => {
+    const searchkey = req.query.search; // fixed typo
+    const query = {
+        language: { $regex: searchkey, $options: "i" } // properly closed
+    };
 
-  const allProjects = await projects.find(query)
-  res.status(200).json(allProjects)
-
-    }catch(err){
-        res.status(401).json(err)
+    try {
+        const allProjects = await projects.find(query);
+        res.status(200).json(allProjects);
+    } catch (err) {
+        res.status(500).json({ error: err.message }); // 401 is for unauthorized, not server errors
     }
-}
+};
+
 
 // home project
-exports.HomeProject = async (req,res)=>{
-    try{
+exports.HomeProject = async (req, res) => {
+    try {
         const aLLhomeproject = await projects.find().limit(3)
-res.status(200).json(aLLhomeproject)
-    }catch(err){
+        res.status(200).json(aLLhomeproject)
+    } catch (err) {
         res.status(401).json(err)
     }
 }
